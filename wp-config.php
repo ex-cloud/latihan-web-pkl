d71e4b7c8997f3b3897fa025a79e1d646115ce93
excloud <andiansyah0425@gmail.com>
2025-07-04 11:22:16 +0700

    update wp-config

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,7 @@
  *
  * @package WordPress
  */
-
+// update wp-config
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'db_latihan_web' );
